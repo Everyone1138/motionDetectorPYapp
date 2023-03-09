76be09b24cc3a847e66aec7b115e66c35281f7b0
campos.santiago138@gmail.com <campos.santiago138@gmail.com>
2023-03-09 12:31:05 -0800

    added imwrite to make the app save the new resized image

--- script.py
+++ script.py
@@ -9,5 +9,6 @@ print(img.ndim)
 
 resize_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
 cv2.imshow("red",resize_image)
+cv2.imwrite("red.jpg", resize_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
