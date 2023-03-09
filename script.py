d38b82d88da6f0712d6167a4bd20f74b9f7ccbbe
campos.santiago138@gmail.com <campos.santiago138@gmail.com>
2023-03-09 12:28:02 -0800

    added image. added py code that shows integers that the ide/computer use to store and read the images. when you run the app the image pops up resized to half it's original size

---
+++ script.py
@@ -0,0 +1,13 @@
+import cv2
+
+img=cv2.imread("red.jpg",0)
+
+print(type(img))
+print(img)
+print(img.shape)
+print(img.ndim)
+
+resize_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
+cv2.imshow("red",resize_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
